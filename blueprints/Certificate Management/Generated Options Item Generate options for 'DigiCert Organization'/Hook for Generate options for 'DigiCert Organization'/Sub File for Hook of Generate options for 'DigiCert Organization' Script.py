2e32a3f67ecd094715349b0574bbac76ac769e50
Shweta Pawar <admin@cloudbolt.io>
2024-11-07 07:06:50 +0000

    6th push

--- blueprints/Certificate Management/Generated Options Item Generate options for 'DigiCert Organization'/Hook for Generate options for 'DigiCert Organization'/Sub File for Hook of Generate options for 'DigiCert Organization' Script.py
+++ blueprints/Certificate Management/Generated Options Item Generate options for 'DigiCert Organization'/Hook for Generate options for 'DigiCert Organization'/Sub File for Hook of Generate options for 'DigiCert Organization' Script.py
@@ -10,18 +10,22 @@ def get_options_list(field, **kwargs):
     for more examples: https://github.com/CloudBoltSoftware/cloudbolt-forge/tree/master/actions/cloudbolt_plugins
     """
 
-   ci = ConnectionInfo.objects.get(name=Digicert API)
-   
-   headers = {
-       'content-Type': 'application/json',
-       'X-DC-DEVKEY': ci.password
-   }
-   
-   response = requests.get(f"{ ci.ip }/organization?filters[status]=active&include_validation=true", headers=headers)
-   result = response.json()
-   options = [item['display_name'] for item in result['organizations'] if 'display_name' in item]
-#   options.insert(0, 'Deutsche Vermögensberatung Aktiengesellschaft DVAG')
-   options.insert(0, "Schön")
+    # ci = ConnectionInfo.objects.get(name="Digicert API")
+    
+    # headers = {
+    #     'content-Type': 'application/json',
+    #     'X-DC-DEVKEY': ci.password
+    # }
+    
+    # response = requests.get(f"{ ci.ip }/organization?filters[status]=active&include_validation=true", headers=headers)
+    # result = response.json()
+    # options = [item['display_name'] for item in result['organizations'] if 'display_name' in item]
+    #   options.insert(0, 'Deutsche Vermögensberatung Aktiengesellschaft DVAG')
+    options = []
+    options.append("A")
+    options.append("B")
+    options.append("C")
+    options.insert(0, "Schön")
 
     return {
         'options': options,
